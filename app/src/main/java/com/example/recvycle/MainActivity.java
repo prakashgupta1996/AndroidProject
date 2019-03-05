7421a1bd5683f7ced391fdf0c015b3d85486e418
pc17 <prakashgupta01133@gmail.com>
2019-03-05 14:37:41 +0530

    pav added

--- app/src/main/java/com/example/recvycle/MainActivity.java
+++ app/src/main/java/com/example/recvycle/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.recvycle;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name", "pav");
+
+        db.collection("food").add(map);
         setContentView(R.layout.activity_main);
     }
 }
